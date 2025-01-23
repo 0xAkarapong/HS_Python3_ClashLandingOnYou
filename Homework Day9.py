804b79c4a1747f2204539d46c03ac2b13cdae03c
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-01-23 21:42:36 +0700

    Refactor centroids and implement point generation logic.
    Updated `real_centroids` to use tuples for clarity and corrected variable name consistency. Implemented `generate_point_around_centroid` function to generate points around centroids and populated `all_points` list accordingly.

--- Homework Day9.py
+++ Homework Day9.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-real_centroids, number_of_centroids =  [np.random.randint(0, 10),
-                                        np.random.randint(0, 10),
-                                        np.random.randint(0, 10),
-                                        np.random.randint(0, 10)], 4
+real_centroids, number_of_centroids =  [(np.random.randint(0, 10),np.random.randint(0, 10)),
+                                        (np.random.randint(0, 10),np.random.randint(0, 10)),
+                                        (np.random.randint(0, 10),np.random.randint(0, 10)),
+                                        (np.random.randint(0, 10),np.random.randint(0, 10))], 4
 
-points_per_centroids = 300
+points_per_centroid = 300
+all_points = []
 
-def generete_point_around_centroid(centroid):
-    pass
+def generate_point_around_centroid(centroid: tuple) -> tuple:
+    x_centroid, y_centroid = centroid
+    x = np.random.normal(x_centroid, 1)
+    y = np.random.normal(y_centroid, 1)
+    return x, y
+
+for centroid in real_centroids:
+    for _ in range(points_per_centroid):
+        all_points.append(generate_point_around_centroid(centroid))
+        # not finished yet
