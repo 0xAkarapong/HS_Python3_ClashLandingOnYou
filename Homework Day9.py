ee8801e0276f36b2c4caf3d973ca55ede22b2dc3
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-01-23 21:26:28 +0700

    Add initial structure for centroid visualization script
    Created a new Python file with placeholder code for generating points around centroids. Includes imports, basic variables, and a function stub for future implementation.

---
+++ Homework Day9.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+real_centroids, number_of_centroids =  [np.random.randint(0, 10),
+                                        np.random.randint(0, 10),
+                                        np.random.randint(0, 10),
+                                        np.random.randint(0, 10)], 4
+
+points_per_centroids = 300
+
+def generete_point_around_centroid(centroid):
+    pass
